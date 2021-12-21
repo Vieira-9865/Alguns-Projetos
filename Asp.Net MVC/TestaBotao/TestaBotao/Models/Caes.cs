0db720e64338f671f61220b71c8091f45f8cf2e0
oelementox10 <oelementox10@gmail.com>
2021-12-21 15:03:05 -0300

    f

---
+++ Asp.Net MVC/TestaBotao/TestaBotao/Models/Caes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TestaBotao.Models
+{
+	public class Caes
+	{
+		[Key]
+
+		public int IdCao { get; set; }
+		[DisplayName("Dono")]
+		public int IdDono { get; set; }
+		[DisplayName("Nome do Cão")]
+		public string Nome_Cao { get; set; }
+		[DisplayName("Nome do Dono")]
+		public string Nome_Dono { get; set; }
+		[DisplayName("Nome da raça")]
+		public string Raca { get; set; }
+		[DisplayName("Nome da raça")]
+		public int IdRaca { get; set; }
+
+	}
+}
---
+++ Asp.Net MVC/TestaBotao/TestaBotao/Models/Racas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TestaBotao.Models
+{
+	public class Racas
+	{
+		[Key]
+		public int IdRaca { get; set; }
+		public string Raca { get; set; }
+
+	}
+}
